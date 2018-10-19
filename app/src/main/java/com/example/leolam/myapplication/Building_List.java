0d9a2f44b7f7b814db7a5f04b8e0a6cd2a0930cd
VaPhi <vphi@uncc.edu>
2018-10-19 02:06:02 -0400

    populated spinners, connect events page to create event page

--- app/src/main/java/com/example/leolam/myapplication/Building_List.java
+++ app/src/main/java/com/example/leolam/myapplication/Building_List.java
@@ -1,7 +1,13 @@
 package com.example.leolam.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.os.Bundle;
+
 
 public class Building_List extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class Building_List extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building__list);
+
+        Spinner spinner = (Spinner) findViewById(R.id.building_list);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.building_list_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
+
 }
--- app/src/main/java/com/example/leolam/myapplication/Events.java
+++ app/src/main/java/com/example/leolam/myapplication/Events.java
@@ -2,6 +2,19 @@ package com.example.leolam.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.icu.util.Freezable;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Events extends AppCompatActivity {
 
@@ -9,5 +22,15 @@ public class Events extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        Button Create_events = (Button)findViewById(R.id.create_event);
+
+        //Contact Button to Contact Page
+        Create_events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup = new Intent(Events.this, Create_events.class);
+                startActivity(signup);
+            }
+        });
     }
 }
